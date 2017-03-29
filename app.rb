300d63f42f484f2d7a2b38283e33918337bbfbf1
Freddy Fallon <freddyfallon@gmail.com>
2017-03-29 10:10:14 +0100

    Revert back to Getting test infrastructure set up

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ class Battle < Sinatra::Base
     'Testing infrastructure working!'
   end
 
-  get '/fighters-form' do
-    erb :index
-  end
-
     # start the server if ruby file executed directly
     run! if app_file == $0
 
