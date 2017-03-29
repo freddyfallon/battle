ea8b91574cec3b6d4c171740455d3780756c9bfe
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-29 12:16:17 +0100

    Test Helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,7 @@
 feature 'Enter names' do
 
   scenario 'can view names after entering them on a form' do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Steve'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John vs. Steve'
   end
 end
