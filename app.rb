35d71bdf06b5fb01b7fff22d7ab50ac95d9d4351
Freddy Fallon <freddyfallon@gmail.com>
2017-03-30 16:15:13 +0100

    Add killing the global variable

--- app.rb
+++ app.rb
@@ -12,17 +12,19 @@ class Battle < Sinatra::Base
   post '/names' do
     @player_1 = Player.new(params[:player_1])
     @player_2 = Player.new(params[:player_2])
-    $game = Game.new(@player_1,@player_2)
+    @game = Game.create(@player_1,@player_2)
     redirect '/player_1_turn'
   end
 
+  before do
+    @game = Game.instance
+  end
+  
   get '/player_1_turn' do
-    @game = $game
     erb(:player_1_turn)
   end
 
   get '/attack_player_2' do
-    @game = $game
     @game.attack(@game.other_player)
     @game.switch_turns
     erb(:attack_player_2)
