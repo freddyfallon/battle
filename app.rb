058074f4d01373b033c10c3ec9f710ede76e9fca
Edward Ward <edwardwardward@gmail.com>
2017-03-30 10:52:44 +0100

    Skinny Controllers

--- app.rb
+++ app.rb
@@ -10,25 +10,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    @player_1 = Player.new(params[:player_1])
+    @player_2 = Player.new(params[:player_2])
+    $game = Game.new(@player_1,@player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_2_hp = $player_2.hit_points
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    Game.new.attack(@player_2)
-    @player_2_hp = $player_2.hit_points
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,15 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player) { double(:player) }
+  subject(:game) { described_class.new(bill,bob) }
+  let(:bill) { double(:player) }
+  let(:bob) { double(:player) }
+
   describe '#attack' do
     it 'damages the player' do
-      expect(player).to receive(:receive_damage)
-      game.attack(player)
+      expect(bob).to receive(:receive_damage)
+      game.attack(bob)
     end
   end
+
 end
